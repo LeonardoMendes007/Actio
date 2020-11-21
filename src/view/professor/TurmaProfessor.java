658e0cdc812bb8aab1bbfba9bb3f8a0df3c8c926
Leonardo Mendes <mendesleo98@gmail.com>
2020-11-21 02:32:18 -0300

    Arrumei o Scroll da Tela TurmaProfessor.

--- src/view/professor/TurmaProfessor.java
+++ src/view/professor/TurmaProfessor.java
@@ -1,21 +1,17 @@
 package view.professor;
 
-
-
 import java.util.ArrayList;
 
 import controller.professor.TurmaProfessorController;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.TilePane;
 import javafx.scene.text.FontWeight;
 import view.Util;
@@ -24,192 +20,182 @@ import view.card.turma.CardTurma;
 public class TurmaProfessor {
 
 	private BorderPane borderPrincipal;
-	
+
 	private GridPane gridTelaTurma;
-	
+
 	private Label lblTurma;
-	
+
 	private TilePane tileDisciplina;
-		
+
 	private ScrollPane scrollDisciplina;
-	
+
 	private GridPane gridDisciplina;
-	
+
 	private ScrollPane scrollTurma;
-	
+
 	private Label lblNomeDisciplina;
-	
-	private HBox hBoxScroll;
-	
+
 	private HBox hBoxTurma;
 
-	//Isso é temporário 
+	// Isso é temporário
 	ArrayList<String> disciplinasExistentes = new ArrayList<String>();
-	
+
 	private TurmaProfessorController controller = new TurmaProfessorController(this);
 
 	public TurmaProfessor(BorderPane border) {
 		this.borderPrincipal = border;
-		
+
 		initTela();
 	}
 
 	public void initTela() {
-		
+
 		initGridDisciplina();
-		
+
 		initLabelDisciplina();
-		
+
 		initScrollDisciplina();
-		
+
 		initEventResponsivel();
-		
+
 		initTileDisciplina();
-		
+
 		controller.addCards();
-		
+
 	}
 
 	private void initEventResponsivel() {
-		
-		//gridTelaDisciplina.heightProperty().addListener((x) -> gridDisciplina.setPrefHeight());
+
+		// gridTelaDisciplina.heightProperty().addListener((x) ->
+		// gridDisciplina.setPrefHeight());
 		gridTelaTurma.widthProperty().addListener((x) -> gridDisciplina.setPrefWidth(gridTelaTurma.getWidth() * 0.9));
-	
-	
+
 	}
 
 	private void initTileDisciplina() {
-	    tileDisciplina = new TilePane(10, 10);
-	    tileDisciplina.setOrientation(Orientation.VERTICAL);
-	    tileDisciplina.setPrefColumns(1);
+		tileDisciplina = new TilePane(10, 10);
+		tileDisciplina.setOrientation(Orientation.VERTICAL);
+		tileDisciplina.setPrefColumns(1);
+		tileDisciplina.setPrefWidth(borderPrincipal.getWidth() * 0.95);
 
-	    
-	    scrollDisciplina.setContent(tileDisciplina);
-		
+		borderPrincipal.widthProperty()
+				.addListener((x) -> tileDisciplina.setPrefWidth(borderPrincipal.getWidth() * 0.95));
+
+		scrollDisciplina.setContent(tileDisciplina);
+
 	}
 
 	private void initScrollDisciplina() {
-		
+
 		scrollDisciplina = new ScrollPane();
-		scrollDisciplina.setPrefSize(2000, 2000);
 		scrollDisciplina.setHbarPolicy(ScrollBarPolicy.NEVER);
 		scrollDisciplina.setPadding(new Insets(20, 0, 0, 0));
-		
+
 		scrollDisciplina.setStyle("-fx-background-color:transparent;");
-	    gridTelaTurma.add(scrollDisciplina, 0, 1);
+		gridTelaTurma.add(scrollDisciplina, 0, 1);
 	}
 
 	private void initLabelDisciplina() {
-		
+
 		lblTurma = new Label("Turma");
-		
+
 		lblTurma.setStyle("-fx-font-weight: bolder; -fx-font-family: Poppins; " + "-fx-font-size: " + 25 + " ;");
-		
+
 		gridTelaTurma.add(lblTurma, 0, 0);
 	}
 
 	private void initGridDisciplina() {
-		
+
 		gridTelaTurma = new GridPane();
-		
+
 		gridTelaTurma.setPadding(new Insets(0, 0, 5, 20));
-		
+
 		borderPrincipal.setCenter(gridTelaTurma);
 		borderPrincipal.setMargin(gridTelaTurma, new Insets(-30, 15, 0, 20));
-		
-		
-		
+
 	}
-	
+
 	private void initGridNomeDisciplina() {
 		gridDisciplina = new GridPane();
 		gridDisciplina.setVgap(5);
 		gridDisciplina.setHgap(10);
 		gridDisciplina.setStyle("-fx-background-radius: 20px; -fx-border-radius: 20px; -fx-background-color: #D7D7D7;");
 		gridDisciplina.setPadding(new Insets(10, 0, 5, 20));
-		
-		
+
+		gridDisciplina.setPrefWidth(borderPrincipal.getWidth()*0.95);
+//		
+		borderPrincipal.widthProperty().addListener((x) -> gridDisciplina.setPrefWidth(borderPrincipal.getWidth()*0.95));
+//		
+
 		tileDisciplina.getChildren().add(gridDisciplina);
-		
+
 	}
-	
-	
+
 	private void initLabelNomeDisciplina(String disciplina) {
-	
+
 		lblNomeDisciplina = new Label(disciplina);
 		Util.setFontePadrao(new Label[] { lblNomeDisciplina }, 20, FontWeight.BOLD);
-		
+
 		gridDisciplina.add(lblNomeDisciplina, 0, 0);
 	}
-	
-	
-	
+
 	private void initScrollTurma() {
 		scrollTurma = new ScrollPane();
 		scrollTurma.setStyle("-fx-background-color: #D7D7D7;");
+		scrollTurma.setPrefHeight(165);
 
-		hBoxScroll.getChildren().add(scrollTurma);
-		hBoxScroll.setPadding(new Insets(10, 10, 10, 10));
-		hBoxScroll.setHgrow(scrollTurma, Priority.ALWAYS);	
-	}
-	
-	private void initHboxScroll() {
+		scrollTurma.setVbarPolicy(ScrollBarPolicy.NEVER);
+//		gridDisciplina.setGridLinesVisible(true);
 
-		hBoxScroll = new HBox(10);
-		hBoxScroll.setStyle("-fx-background-color: #D7D7D7;");
-		hBoxScroll.setAlignment(Pos.CENTER);
-		hBoxScroll.setPadding(new Insets(0, 0, 20, 0));
-
-		gridDisciplina.add(hBoxScroll, 0, 1);
 		
+		gridDisciplina.add(scrollTurma, 0, 1);
 	}
-	
+
 	private void initHBoxTurma() {
 
 		hBoxTurma = new HBox(10);
 		hBoxTurma.setStyle("-fx-background-color: #D7D7D7;");
 		hBoxTurma.setAlignment(Pos.CENTER);
-		hBoxTurma.setPadding(new Insets(0, 0, 20, 0));
+		hBoxTurma.setPadding(new Insets(5, 0, 15, 0));
 
 		scrollTurma.setContent(hBoxTurma);
 
 	}
-	
+
 	public void initNovaDisciplina(CardTurma card) {
 		initGridNomeDisciplina();
 		initLabelNomeDisciplina(card.getDisciplina());
-		initHboxScroll();
 		initScrollTurma();
 		initHBoxTurma();
 		addCard(card);
 		disciplinasExistentes.add(card.getDisciplina());
 	}
-	
+
 	public void addCard(CardTurma card) {
-		
-		hBoxTurma.getChildren().add(card.getCard());	
+
+		hBoxTurma.getChildren().add(card.getCard());
 	}
-	
+
 	public void addDisciplina(CardTurma card) {
 		boolean existe = false;
-		
-		//Se o vetor está vazio ele já add
-		if(disciplinasExistentes.isEmpty()) {
-			initNovaDisciplina(card);
-		
-		}else{
-			//Caso já exista uma disciplina no vetor ele vai verificar se
-			//precisa criar uma nova, ou é só add mais um card de turma
 
-			for(String disciplina : disciplinasExistentes) {
-				if(card.getDisciplina().equals(disciplina)) {
+		// Se o vetor está vazio ele já add
+		if (disciplinasExistentes.isEmpty()) {
+			initNovaDisciplina(card);
+
+		} else {
+			// Caso já exista uma disciplina no vetor ele vai verificar se
+			// precisa criar uma nova, ou é só add mais um card de turma
+
+			for (String disciplina : disciplinasExistentes) {
+				if (card.getDisciplina().equals(disciplina)) {
 					addCard(card);
 					existe = true;
 				}
 			}
-			
-			if(existe == false) {
-				initNovaDisciplina(card);	
+
+			if (existe == false) {
+				initNovaDisciplina(card);
 			}
 		}
 	}
@@ -217,5 +203,5 @@ public class TurmaProfessor {
 	public BorderPane getBorderPrincipal() {
 		return borderPrincipal;
 	}
-	
+
 }
